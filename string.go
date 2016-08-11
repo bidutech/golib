724e320cdad48b90f2f4c2289b773b32a60dc783
shanhai2015 <393366290@qq.com>
2016-08-11 18:00:27 +0800

    add string function

--- string.go
+++ string.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"sort"
+	"strings"
 	"sync"
 )
 
@@ -108,4 +109,44 @@ func Substr(str string, start, length int) string {
 		end = rl
 	}
 	return string(rs[start:end])
+}
+
+/**
+使用 splitstr 分割 s ，找splitstr在字符串s中最后的位置
+并以此位置进行分割为front和 after两部分 并返回 splitstr所在位置index
+如果没有找到splitstr 则 index 为 -1
+
+**/
+func StrSplit(s string, splitstr string) (front, after string, index int) {
+
+	n := strings.Index(s, splitstr)
+	index = n
+	if n < 0 {
+		front = s
+		after = ""
+		return
+	}
+	front = Substr(s, 0, n)
+	after = Substr(s, n, len(s))
+	return
+}
+
+/**
+使用 splitstr 分割 s ，找splitstr在字符串s中最后的位置
+并以此位置进行分割为front和 after两部分 并返回 splitstr所在位置index
+如果没有找到splitstr 则 index 为 -1
+
+**/
+func StrLastSplit(s string, splitstr string) (front, after string, index int) {
+
+	n := strings.LastIndex(s, splitstr)
+	index = n
+	if n < 0 {
+		front = s
+		after = ""
+		return
+	}
+	front = Substr(s, 0, n)
+	after = Substr(s, n, len(s))
+	return
 }
